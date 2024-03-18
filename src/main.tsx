b2a761d0fe0de7d7174fcd4b9df41cfa6268ce0e
vendredl <node.js_@kakao.com>
2024-03-19 00:13:29 +0900

    :speech_balloon: Provider 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { RouterProvider } from "react-router-dom";
+import router from "./router/Router.tsx";
+import { RecoilRoot } from "recoil";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<RecoilRoot>
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+			<App />
+		</QueryClientProvider>
+	</RecoilRoot>
+);
