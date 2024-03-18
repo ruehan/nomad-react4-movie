233d0367dee52b77e8553c8ff36e39a7ae03a2f5
vendredl <node.js_@kakao.com>
2024-03-19 01:01:56 +0900

    :lipstick: 스타일 추가

--- src/pages/MoviePage.tsx
+++ src/pages/MoviePage.tsx
@@ -1,7 +1,57 @@
 import { useQuery } from "react-query";
-import { getComingSoon, getNowPlaying, getPopular } from "../api/api";
+import {
+	getComingSoon,
+	getNowPlaying,
+	getPopular,
+	makeImagePath,
+} from "../api/api";
 import { useRecoilState } from "recoil";
 import { tabState } from "../state/tabState";
+import { styled } from "styled-components";
+import { motion } from "framer-motion";
+
+const MovieContainer = styled(motion.div)`
+	display: flex;
+	/* width: 90%; */
+	/* height:  */
+	margin-top: 100px;
+	justify-content: center;
+	align-items: center;
+	flex-wrap: wrap;
+`;
+
+const Movie = styled(motion.div)`
+	display: flex;
+	flex-direction: column;
+	min-width: 200px;
+	max-width: 200px;
+	min-height: 300px;
+	/* background-color: orange; */
+	margin: 20px;
+	text-align: center;
+`;
+
+const Img = styled(motion.img)`
+	width: 200px;
+	height: auto;
+	border-radius: 15px;
+`;
+
+const Text = styled.p`
+	margin-top: 20px;
+`;
+
+const container = {
+	hidden: { opacity: 1, scale: 0 },
+	visible: {
+		opacity: 1,
+		scale: 1,
+		transition: {
+			delayChildren: 0.3,
+			staggerChildren: 0.2,
+		},
+	},
+};
 
 const MoviePage: React.FC = () => {
 	const [tab, setTab] = useRecoilState(tabState);
@@ -9,8 +59,8 @@ const MoviePage: React.FC = () => {
 	const { isLoading, isError, data, error } = useQuery(
 		tab === "POPULAR"
 			? "popular"
-			: tab === "COMMING SOON"
-			? "comming-soon"
+			: tab === "COMING SOON"
+			? "coming-soon"
 			: "now-playing",
 		tab === "POPULAR"
 			? getPopular
@@ -21,22 +71,45 @@ const MoviePage: React.FC = () => {
 			refetchOnWindowFocus: false,
 			retry: 0,
 			onSuccess: (data) => {
-				// console.log(data.results);
+				console.log(data.results);
 			},
 			onError: (e) => {
-				console.log("error");
+				// console.log(e.message);
 			},
 		}
 	);
 
+	const item = {
+		hidden: { y: 20, opacity: 0 },
+		visible: {
+			y: 0,
+			opacity: 1,
+		},
+	};
+
+	const img = {
+		whileHover: {
+			scale: 1.1,
+			y: -50,
+		},
+	};
+
 	if (isLoading) return null;
 
 	return (
-		<div>
+		<MovieContainer
+			key={tab}
+			variants={container}
+			initial="hidden"
+			animate="visible"
+		>
 			{data.results.map((movie: any) => (
-				<p key={movie.id}>{movie.title}</p>
+				<Movie key={movie.id} variants={item}>
+					<Img src={makeImagePath(movie.poster_path)}></Img>
+					<Text>{movie.title}</Text>
+				</Movie>
 			))}
-		</div>
+		</MovieContainer>
 	);
 };
 
