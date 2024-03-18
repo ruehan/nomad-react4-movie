329816b57e3e8b432457dc4a795cc4e6cc08587c
vendredl <node.js_@kakao.com>
2024-03-19 00:54:18 +0900

    :bug: LocalStorage에 Tab 데이터 추가

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -1,14 +1,16 @@
 import { motion } from "framer-motion";
-import { Link, useMatch } from "react-router-dom";
+import { Link, Outlet, useMatch } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import { styled } from "styled-components";
 import { tabState } from "../../state/tabState";
+import { useEffect } from "react";
 
 const Tabs = styled.ul`
 	width: 100%;
 	height: 50px;
 	/* background-color: white; */
 	display: flex;
+	margin-top: 20px;
 	justify-content: space-around;
 `;
 
@@ -22,7 +24,7 @@ const Tab = styled.li`
 
 const Circle = styled(motion.span)`
 	position: absolute;
-	width: 15px;
+	width: 60px;
 	height: 7px;
 	background-color: #ec6b5d;
 	bottom: -15px;
@@ -40,37 +42,50 @@ const NavBar: React.FC = () => {
 	const nowPlayingMatch = useMatch("now-playing");
 	const myMatch = useMatch("my");
 
+	useEffect(() => {
+		try {
+			const tab = localStorage.getItem("tab") as string;
+			setTab(tab);
+		} catch {
+			console.log("nothing");
+		}
+	}, []);
+
 	const onClick = (event: any) => {
 		setTab(event.target.innerText);
+		localStorage.setItem("tab", event.target.innerText);
 	};
 
 	return (
-		<Tabs>
-			<Tab>
-				<Link to="/" onClick={onClick}>
-					POPULAR
-					{popularMatch && <Circle layoutId="circle" />}
-				</Link>
-			</Tab>
-			<Tab>
-				<Link to="/coming-soon" onClick={onClick}>
-					COMING SOON
-					{comingSoonMatch && <Circle layoutId="circle" />}
-				</Link>
-			</Tab>
-			<Tab>
-				<Link to="/now-playing" onClick={onClick}>
-					NOW PLAYING
-					{nowPlayingMatch && <Circle layoutId="circle" />}
-				</Link>
-			</Tab>
-			<Tab>
-				<Link to="/my" onClick={onClick}>
-					MY
-					{myMatch && <Circle layoutId="circle" />}
-				</Link>
-			</Tab>
-		</Tabs>
+		<>
+			<Tabs>
+				<Tab>
+					<Link to="/" onClick={onClick}>
+						POPULAR
+						{popularMatch && <Circle layoutId="circle" />}
+					</Link>
+				</Tab>
+				<Tab>
+					<Link to="/coming-soon" onClick={onClick}>
+						COMING SOON
+						{comingSoonMatch && <Circle layoutId="circle" />}
+					</Link>
+				</Tab>
+				<Tab>
+					<Link to="/now-playing" onClick={onClick}>
+						NOW PLAYING
+						{nowPlayingMatch && <Circle layoutId="circle" />}
+					</Link>
+				</Tab>
+				<Tab>
+					<Link to="/my" onClick={onClick}>
+						MY
+						{myMatch && <Circle layoutId="circle" />}
+					</Link>
+				</Tab>
+			</Tabs>
+			{/* <Outlet /> */}
+		</>
 	);
 };
 
