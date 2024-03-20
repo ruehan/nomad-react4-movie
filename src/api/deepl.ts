29c31d38a560df15241e08e406df0761879b9ecc
vendredl <node.js_@kakao.com>
2024-03-20 20:42:30 +0900

    :alien: DEEPL API 수정

--- src/api/deepl.ts
+++ src/api/deepl.ts
@@ -1,13 +1,18 @@
 import axios from "axios";
+import { useRecoilState } from "recoil";
+import { translateState, translateTextState } from "../state/movieState";
 
 const translateText = async (text: any) => {
-	const AUTH_KEY = "c583fbb3-67c3-4db8-a85c-c4fc3e509134:fx";
+	const [, isTranslate] = useRecoilState(translateState);
+	const [, setTranslatedText] = useRecoilState(translateTextState);
+
+	const AUTH_KEY = process.env.REACT_APP_DEEPL_AUTH_KEY;
 
 	const url = `https://api-free.deepl.com/v2/translate?auth_key=${AUTH_KEY}&text=${text}&source_lang=EN&target_lang=KO`;
 
-	axios
-		.get(url)
-		.then((response) => console.log(response.data.translations[0].text));
+	axios.get(url).then((response) => {
+		setTranslatedText(response.data.translations[0].text);
+	});
 };
 
 export default translateText;
